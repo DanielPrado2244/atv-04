9db42fcef70b90c5192ea69ea04754d64d6c0b25
Daniel <danielprado206@yahoo.com.br>
2020-05-09 00:23:36 -0300

    projeto finalizado

---
+++ Atv_04/src/java/SubmitAttributes.java
@@ -0,0 +1,69 @@
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns = {"/SubmitAttributes"})
+public class SubmitAttributes extends HttpServlet {
+
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        
+        String pReq = request.getParameter("req");
+        String pSes = request.getParameter("ses");
+        String pApp = request.getParameter("app");
+
+        request.setAttribute("flu1", pReq); // Setando no escopo de requisição
+        request.getSession().setAttribute("flu2", pSes); // Setando no escopo de sessão.
+        getServletContext().setAttribute("flu3", pApp); // Setando no escopo de aplicação.
+
+        RequestDispatcher view = request.getRequestDispatcher("listaAtributos.jsp");
+        view.forward(request, response);
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
